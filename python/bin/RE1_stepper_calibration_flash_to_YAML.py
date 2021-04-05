098e7721a4bef4910543dd5984bd22fca4548a6b
Aaron Edsinger <aedsinger@hello-robot.com>
2021-04-04 20:29:16 -0700

    updated tools to exit on protocol mismatch

--- python/bin/RE1_stepper_calibration_flash_to_YAML.py
+++ python/bin/RE1_stepper_calibration_flash_to_YAML.py
@@ -10,10 +10,11 @@ if len(sys.argv) < 2:
     raise Exception("Provide motor name e.g.: stepper_calibration_flash_to_YAML.py hello-motor1")
 motor_name = sys.argv[1]
 motor = stepper.Stepper('/dev/'+motor_name)
-motor.startup()
+if not motor.startup():
+    exit()
 data = motor.read_encoder_calibration_from_flash()
-print 'Read data of len',len(data)
-print 'Writing calibration data to YAML...'
+print('Read data of len',len(data))
+print('Writing calibration data to YAML...')
 motor.write_encoder_calibration_to_YAML(data)
 
 
