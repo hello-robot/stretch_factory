352eda1b1a4c9084dd22b45c378f3a1e7785c285
Aaron Edsinger <aedsinger@hello-robot.com>
2021-05-25 09:36:58 -0700

    Reverted DXL tools to not set baud rates. Functionality belongs in Develop branch instead

--- python/bin/RE1_dynamixel_reboot.py
+++ python/bin/RE1_dynamixel_reboot.py
@@ -6,13 +6,12 @@ import argparse
 
 parser=argparse.ArgumentParser(description='Reboot all of the Dynamixel servos on a bus')
 parser.add_argument("usb", help="The dynamixel USB bus e.g.: /dev/hello-dynamixel-head")
-parser.add_argument("--baud", help="Baud rate (57600, 115200, or 1000000) [57600]", type=int,default=57600)
 args = parser.parse_args()
 
 m=None
 try:
     for id in range(25):
-        m = DynamixelXL430(id, args.usb,baud=args.baud)
+        m = DynamixelXL430(id, args.usb)
         m.startup()
         if (m.do_ping(verbose=True)):
             m.do_reboot()
