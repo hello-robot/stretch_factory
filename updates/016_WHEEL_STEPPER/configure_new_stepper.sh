8c5d2e8e9b36f156e56866718b1a6a52483bee94
Aaron Edsinger <aedsinger@hello-robot.com>
2021-11-05 16:19:23 -0700

    016 update

---
+++ updates/016_WHEEL_STEPPER/configure_new_stepper.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo chmod a+rw $HELLO_FLEET_PATH/$HELLO_FLEET_ID/udev/*
+sudo cp *.rules $HELLO_FLEET_PATH/$HELLO_FLEET_ID/udev
+sudo cp hello-motor*.yaml $HELLO_FLEET_PATH/$HELLO_FLEET_ID/calibration_steppers
+sudo cp *.rules /etc/udev/rules.d
+sudo cp *.rules /etc/hello-robot/$HELLO_FLEET_ID/udev
+
+
+
+
