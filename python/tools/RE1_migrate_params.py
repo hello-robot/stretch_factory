1c0246fbfe4cf7a970670f9f4cc4a0e2307c6d8a
Aaron Edsinger <aedsinger@hello-robot.com>
2022-05-09 12:05:26 -0700

    RE1_migrate_params working ready for PR

--- python/tools/RE1_migrate_params.py
+++ python/tools/RE1_migrate_params.py
@@ -14,10 +14,9 @@ def dir_path(string):
         exit()
 
 parser = argparse.ArgumentParser(description='Migrate Stretch parameter data to latest format')
-group = parser.add_mutually_exclusive_group()
-group.add_argument("--no_prompt", help="Don't use prompts", action="store_true")
-group.add_argument("--mgmt", help="Display overview on parameter management", action="store_true")
-group.add_argument("--path", help="Path to robot parameters (if not $HELLO_FLEET_PATH/$HELLO_FLEET_ID)", type=dir_path)
+parser.add_argument("--no_prompt", help="Don't use prompts", action="store_true")
+parser.add_argument("--diff", help="Check differences of proposed migrated params to current params", action="store_true")
+parser.add_argument("--path", help="Path to robot parameters (if not $HELLO_FLEET_PATH/$HELLO_FLEET_ID)", type=dir_path)
 args = parser.parse_args()
 
 if args.path is None:
@@ -28,67 +27,91 @@ fleet_dir=fleet_dir.rstrip('/')
 fleet_path=fleet_dir[:-17]
 fleet_id=fleet_dir[-16:]
 
+def cleanup_generated_files():
+    # Cleanup
+    if exists(hello_utils.get_fleet_directory() + 'stretch_user_params.yaml'):
+        os.system('rm %s' % (hello_utils.get_fleet_directory() + 'stretch_user_params.yaml'))
+    if exists(hello_utils.get_fleet_directory() + 'stretch_configuration_params.yaml'):
+        os.system('rm %s' % (hello_utils.get_fleet_directory() + 'stretch_configuration_params.yaml'))
+
 # Point to the data to be migrated
 hello_utils.set_fleet_directory(fleet_path, fleet_id)
-
-print('Checking parameters found at: %s'%fleet_dir)
+click.secho('############## Migrating  %s ###############'%fleet_id,fg="green")
+print('Checking parameters  at: %s'%fleet_dir)
 print('-------------------------------------')
-if exists(hello_utils.get_fleet_directory()+'stretch_user_params.yaml') and exists(hello_utils.get_fleet_directory()+'stretch_configuration_params.yaml'):
-    print('Parameter format is up to date. No migration is required.')
-    exit()
 
-if click.confirm('Migration is required for robot %s. Proceed?'%fleet_id):
-    if 1:#try:
-        O, U, R=param_mgmt.migrate_params_RE1P0(fleet_path, fleet_id)
+if exists(hello_utils.get_fleet_directory()+'stretch_user_params.yaml') and exists(hello_utils.get_fleet_directory()+'stretch_configuration_params.yaml'):
+    click.secho('Parameter format is up to date. No migration is required.', fg="green")
+    exit(0)
+if not exists(hello_utils.get_fleet_directory()+'stretch_re1_user_params.yaml') or not exists(hello_utils.get_fleet_directory()+'stretch_re1_factory_params.yaml'):
+    click.secho('Robot paramters not found. Unable to migrate', fg="red")
+    exit(1)
+
+if args.diff or click.confirm('Migration is required for robot %s. Proceed?'%fleet_id):
+    if 1: #try:
+        drop_user_params = []#['factory_params', 'tool_params']
+        O, U, R=param_mgmt.migrate_params_RE1P0(fleet_path, fleet_id,drop_user_params)
+        if O==None:#Failed
+            click.secho('Robot paramters corrupted. Unable to migrate', fg="red")
+            exit(1)
         print('Migration complete. Starting validation...')
 
         #Read in the new parameter data
         import stretch_body.robot_params
         (UU, RR) = stretch_body.robot_params.RobotParams().get_params()
 
+        #These parameters are whitelisted (OK'd) to be added/dropped/changed during migration after manual review of the migration process
+        #of all robots up to an including Louis
+
+        #Parameters that have been introduced since the customer yaml was generated
+        added_whitelist=['head_pan.stall_backoff']
+
+        #These are parameters that have been deperecated so we're OK dropping them from the new yaml
+        dropped_whitelist=['factory_params','tool_params', 'robot.use_arm', 'robot.use_wacc', 'robot.use_lift', 'robot.use_end_of_arm', 'robot.use_head', 'robot.use_pimu','robot.use_base' ]
+
+        #We are OK changing these parameters on the customer during migration as we need to take back control of the ability to update them via Pip
+        change_whitelist=['head_tilt.pid','pimu.config.bump_thresh','base.contact_thresh_max_N','hello-motor-lift.gains.i_safety_feedforward','logging.handlers.file_handler.filename',
+                          'pimu.config.stop_at_low_voltage']
+
         # Now check for differences
-        added_warnings = param_mgmt.param_added_check(RR, R, 0, 'NewParams', 'OldParams',whitelist=['stall_backoff'])
-        dropped_warnings = param_mgmt.param_dropped_check(RR, R, 0, 'NewParams', 'OldParams',whitelist=['factory_params'])
-        change_warnings = param_mgmt.param_change_check(RR, R, 0, 'NewParams', 'OldParams',whitelist=['pid'])
 
-        print('Validation check: Added %d, Dropped %d, Changed %d' % (added_warnings, dropped_warnings, change_warnings))
-        print('Validation check should report 0 warnings. Reported total of %d' % (added_warnings+dropped_warnings+change_warnings))
-        if click.confirm('Use new parameters for robot %s?'%fleet_id):
-            uf=hello_utils.get_fleet_directory()+'stretch_re1_user_params.migration_backup.%s.yaml'%(hello_utils.create_time_string())
-            os.system('mv %s %s' % (hello_utils.get_fleet_directory() + 'stretch_re1_user_params.yaml', uf))
-            print('Moving %s to %s' % (hello_utils.get_fleet_directory() + 'stretch_re1_user_params.yaml', uf))
-            ff = hello_utils.get_fleet_directory() + 'stretch_re1_factory_params.migration_backup.%s.yaml' % (hello_utils.create_time_string())
-            os.system('mv %s %s' % (hello_utils.get_fleet_directory() + 'stretch_re1_factory_params.yaml', ff))
-            print('Moving %s to %s' % (hello_utils.get_fleet_directory() + 'stretch_re1_factory_params.yaml', ff))
-            print("Robot %s now configured to use latest parameter format"%fleet_id)
+        added_warnings = param_mgmt.param_added_check(RR, R, 0, 'NewParams', 'OldParams',whitelist=added_whitelist)
+        dropped_warnings = param_mgmt.param_dropped_check(RR, R, 0, 'NewParams', 'OldParams',whitelist=dropped_whitelist)
+        change_warnings = param_mgmt.param_change_check(RR, R, 0, 'NewParams', 'OldParams',whitelist=change_whitelist)
+
+        if added_warnings+dropped_warnings+change_warnings>0:
+            color='red'
+            ret=1
         else:
-            uf=fleet_path+'/log/stretch_user_params.migration_backup.%s.%s.yaml'%(fleet_id,hello_utils.create_time_string())
-            os.system('mv %s %s'% (hello_utils.get_fleet_directory()+'stretch_user_params.yaml',uf))
-            print('Moving %s to %s'%(hello_utils.get_fleet_directory()+'stretch_user_params.yaml',uf))
-            cf = fleet_path + '/log/stretch_configuration_params.migration_backup.%s.%s.yaml' % (fleet_id, hello_utils.create_time_string())
-            os.system('mv %s %s' % (hello_utils.get_fleet_directory() + 'stretch_configuration_params.yaml', uf))
-            print('Moving %s to %s' % (hello_utils.get_fleet_directory() + 'stretch_configuration_params.yaml', uf))
+            color='green'
+            ret=0
+        click.secho('Validation check: Added %d, Dropped %d, Changed %d' % (added_warnings, dropped_warnings, change_warnings),fg=color)
+        click.secho('Validation check should report 0 warnings. Reported total of %d' % (added_warnings+dropped_warnings+change_warnings),fg=color)
+
+        if args.diff:
+            cleanup_generated_files()
+            exit(ret)
+        else:
+            if click.confirm('Use new parameters for robot %s?'%fleet_id):
+                uf=hello_utils.get_fleet_directory()+'stretch_re1_user_params.migration_backup.%s.yaml'%(hello_utils.create_time_string())
+                os.system('mv %s %s' % (hello_utils.get_fleet_directory() + 'stretch_re1_user_params.yaml', uf))
+                print('Moving %s to %s' % (hello_utils.get_fleet_directory() + 'stretch_re1_user_params.yaml', uf))
+                ff = hello_utils.get_fleet_directory() + 'stretch_re1_factory_params.migration_backup.%s.yaml' % (hello_utils.create_time_string())
+                os.system('mv %s %s' % (hello_utils.get_fleet_directory() + 'stretch_re1_factory_params.yaml', ff))
+                print('Moving %s to %s' % (hello_utils.get_fleet_directory() + 'stretch_re1_factory_params.yaml', ff))
+                print("Robot %s now configured to use latest parameter format"%fleet_id)
+                #Now copy to /etc / git?
+            else:
+                uf=fleet_path+'/log/stretch_user_params.migration_backup.%s.%s.yaml'%(fleet_id,hello_utils.create_time_string())
+                os.system('mv %s %s'% (hello_utils.get_fleet_directory()+'stretch_user_params.yaml',uf))
+                print('Moving %s to %s'%(hello_utils.get_fleet_directory()+'stretch_user_params.yaml',uf))
+                cf = fleet_path + '/log/stretch_configuration_params.migration_backup.%s.%s.yaml' % (fleet_id, hello_utils.create_time_string())
+                os.system('mv %s %s' % (hello_utils.get_fleet_directory() + 'stretch_configuration_params.yaml', uf))
+                print('Moving %s to %s' % (hello_utils.get_fleet_directory() + 'stretch_configuration_params.yaml', uf))
+            exit(0)
     # except:
-    #     #Cleanup
-    #     if exists(hello_utils.get_fleet_directory() + 'stretch_user_params.yaml'):
-    #         os.system('rm %s'%hello_utils.get_fleet_directory() + 'stretch_user_params.yaml')
-    #     if exists(hello_utils.get_fleet_directory() + 'stretch_configuration_params.yaml'):
-    #         os.system('rm %s' % hello_utils.get_fleet_directory() + 'stretch_configuration_params.yaml')
+    #     print('Exception. Cleaning up')
+    #     cleanup_generated_files()
+    #     exit(1)
 else:
-    exit()
-
-
-mgmt = """
-PARAMETER MANAGEMENT
---------------------
- 
-"""
-
-
-# if args.check:
-#     f=mgmt.get_robot_parameter_format(fleet_dir)
-#
-# if args.mgmt:
-#     print(mgmt)
-#     exit()
-
+    exit(0)
