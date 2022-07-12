0b6f1caf73898158aff81e7a203aad31cb759244
Blake Werner <blake_werner@berkeley.edu>
2022-07-12 12:10:48 -0700

    almost done

--- python/tools/RE1_model_tools.py
+++ python/tools/RE1_model_tools.py
@@ -6,9 +6,14 @@ from numpy import linalg as LA
 import matplotlib.pyplot as plt
 import math
 
+GRAVITY = 9.81
+VELOCITY_FRICTION_CONST = 100
 
+# This should be full runs from top to bottom
+test_positions = [0, 50, 0, -50, 0]
 
-motor = stepper.Stepper('/dev/hello-motor-lift')
+# This is the arbitrary pause time (in clock cycles)
+pause_time = 2000
 
 def setup_motor():
     # Setup
@@ -29,53 +34,52 @@ def setup_motor():
 #######################START MOTOR MODEL CODE#########################
 
 """Call setup_current_model when motor is on in order to run sweeps and get coefficients for model"""
-"""MOTOR MUST BE SET UP FIRST"""
-def setup_current_model(self):
-    A, b = self.get_least_squares_params()
+def setup_current_model():
+    A, b = get_current_params()
     x = LA.lstsq(A.T, b, rcond=0)[0]
-    self.model_coefficients = x
+    return x
 
-def get_least_squares_params(self):
+def get_current_params():
     print("Starting data gathering")
     current_values = []
     velocity_mg_values = []
     acceleration_mg_values = []
 
-    for destination in self.test_positions:
+    for destination in test_positions:
         print(destination)
-        self.motor.pull_status()
-        self.motor.set_command(x_des=destination)
-        self.motor.push_command()
+        motor.pull_status()
+        motor.set_command(x_des=destination)
+        motor.push_command()
 
         # Gathering data while moving
-        while not self.motor.status['near_pos_setpoint']:
-            self.motor.pull_status()
+        while not motor.status['near_pos_setpoint']:
+            motor.pull_status()
 
-            current_values.append(self.motor.status['current'])
-            velocity_mg_values.append(self.motor.status['vel_mg'])
-            acceleration_mg_values.append(self.motor.status['accel_mg'])
+            current_values.append(motor.status['current'])
+            velocity_mg_values.append(motor.status['vel_mg'])
+            acceleration_mg_values.append(motor.status['accel_mg'])
 
         # A rather hacky way to pause while still generating data
         t = 0
-        while t < self.pause_time:
-            self.motor.pull_status()
+        while t < pause_time:
+            motor.pull_status()
 
-            current_values.append(self.motor.status['current'])
-            velocity_mg_values.append(self.motor.status['vel_mg'])
-            acceleration_mg_values.append(self.motor.status['accel_mg'])
+            current_values.append(motor.status['current'])
+            velocity_mg_values.append(motor.status['vel_mg'])
+            acceleration_mg_values.append(motor.status['accel_mg'])
 
             t = t + 1
 
     # velocity values and previous values
     A_list = [np.array(velocity_mg_values), np.array(acceleration_mg_values)]
 
-    A = self.get_features(A_list)
+    A = get_current_features(A_list)
 
     b = np.array(current_values)
 
     return A, b
 
-def get_features(self, linear_features): ####NOTE THERE ARE FOUR FEATURES, SO STORE AS FOUR FLOATS################
+def get_current_features(linear_features): ####NOTE THERE ARE FOUR FEATURES, SO STORE AS FOUR FLOATS################
     velocity = linear_features[0]
     acceleration = linear_features[1]
     A = np.vstack((np.ones((np.shape(velocity))), velocity,
@@ -90,72 +94,52 @@ def get_features(self, linear_features): ####NOTE THERE ARE FOUR FEATURES, SO ST
 ######################START TORQUE MODEL CODE#####################################
 
 """Set up the coefficients with a few sweeps as usual"""
-def setup_torque_model(self, end_positions):
-    A, b = self.get_torque_params(end_positions)
-    coefficients = self.get_torque_model(A, b)
-    self.torque_coefficients = coefficients
-
-
-
-"""Calculate the expected torque given the speed, current, and acceleration (sometimes?)"""
-def calculate_expected_torque(self):
-    assert self.torque_coefficients is not None
-    self.motor.pull_status()
-    current = self.motor.status['current']
-    vel = self.motor.status['vel_mg']
-    acc = self.motor.status['accel_mg']
-    A_list = [np.array(vel), np.array(current), np.array(acc)]
-
-    A = self.get_torque_features(A_list)
-
-    return np.dot(A, self.torque_coefficients)
-
-
-
-
-######################TORQUE MODEL UTILS#############################
+def setup_torque_model():
+    A, b = get_torque_params()
+    x = LA.lstsq(A.T, b, rcond=0)[0]
+    return x
 
 """Get the A and b by running motor a bunch of times, getting data, getting features from data, and turning into matrix"""
-def get_torque_params(self, test_end_positions):
+def get_torque_params():
     print("Starting data gathering")
     current_values = []
     velocity_mg_values = []
     torque_values = []
     acc_values = []
 
-    for destination in test_end_positions:
+    for destination in test_positions:
         print(destination)
-        self.motor.pull_status()
-        self.motor.set_command(x_des=destination)
-        self.motor.push_command()
+        motor.pull_status()
+        motor.set_command(x_des=destination)
+        motor.push_command()
 
-        while not self.motor.status['near_pos_setpoint']:
-            self.motor.pull_status()
-            current = self.motor.status['current']
-            vel = self.motor.status['vel_mg']
-            acc = self.motor.status['accel_mg']
+        while not motor.status['near_pos_setpoint']:
+            motor.pull_status()
+            current = motor.status['current']
+            vel = motor.status['vel_mg']
+            acc = motor.status['accel_mg']
 
             current_values.append(current)
             velocity_mg_values.append(vel)
             acc_values.append(acc)
 
-            force = (self.grav + acc) * self.mass + self.velocity_friction_constant * np.sign(vel)
-            torque = self.calculate_torque(force)
+            force = (GRAVITY + acc) * self.mass + VELOCITY_FRICTION_CONST * np.sign(vel)
+            torque = calculate_torque(force)
             torque_values.append(torque)
 
         t = 0
-        while t < self.pause_time:
-            self.motor.pull_status()
-            current = self.motor.status['current']
-            vel = self.motor.status['vel_mg']
-            acc = self.motor.status['accel_mg']
+        while t < pause_time:
+            motor.pull_status()
+            current = motor.status['current']
+            vel = motor.status['vel_mg']
+            acc = motor.status['accel_mg']
 
             current_values.append(current)
             velocity_mg_values.append(vel)
             acc_values.append(acc)
 
-            force = (self.grav + acc) * self.mass + self.velocity_friction_constant * np.sign(vel)
-            torque = self.calculate_torque(force)
+            force = (GRAVITY + acc) * self.mass + VELOCITY_FRICTION_CONST * np.sign(vel)
+            torque = calculate_torque(force)
             torque_values.append(torque)
 
             t = t + 1
@@ -166,14 +150,14 @@ def get_torque_params(self, test_end_positions):
 
     A_list = [np.array(velocity_mg_values), np.array(current_values), np.array(acc_values)]
 
-    A = self.get_torque_features(A_list)
+    A = get_torque_features(A_list)
 
     b = np.array(torque_values)
 
     return A, b
 
 """Get possibly higher order features from linear features fed in (current_amplitude, temperature, current_frequency)"""
-def get_torque_features(self, linear_features):
+def get_torque_features(linear_features):
     # A = np.vstack((linear_features[0], linear_features[1]))
     velocity = linear_features[0]
     current = linear_features[1]
@@ -185,19 +169,47 @@ def get_torque_features(self, linear_features):
     return A
 
 """Get the torque from the input force"""
-def calculate_torque(self, force):
-    return force * self.pulley_rad * self.gear_ratio
-
-"""Solve least squares with A and b to get the x such that Ax - b is minimized in L2 norm (can change if needed?)"""
-def get_torque_model(self, A, b):
-    return LA.lstsq(A.T, b, rcond=0)[0]
+def calculate_torque(force):
+    return force * pulley_rad * gear_ratio
 
 
+"""Calculate the expected torque given the speed, current, and acceleration (sometimes?)"""
+def calculate_expected_torque():
+    #TODO Where do torque coefficients come from and go to?
+    assert torque_coefficients is not None
+    motor.pull_status()
+    current = motor.status['current']
+    vel = motor.status['vel_mg']
+    acc = motor.status['accel_mg']
+    A_list = [np.array(vel), np.array(current), np.array(acc)]
+
+    A = get_torque_features(A_list)
+
+    return np.dot(A, torque_coefficients)
+
+
+
+
+
+"""Input arguments are the motor name (just this for now, change?)"""
 if __name__ == "__main__":
+    motor_name = sys.argv[0]
+    # '/dev/hello-motor-lift' in testing
+    mode = int(sys.argv[1])
+    # 0 for current model setup, 1 for torque model setup
+
+    motor = stepper.Stepper(motor_name)
+
+
     try:
         setup_motor()
 
-        #TODO ADD ALL OTHER THINGS HERE
+        if mode == 0:
+            current_coefficients = setup_current_model()
+        elif mode == 1:
+            torque_coefficients = setup_torque_model()
+        else:
+            raise Exception("mode must be 0 or 1")
 
     except (KeyboardInterrupt, SystemExit):
         motor.stop()
