e0af1527a06c4e7c64e6a1910af4f4049891a1a9
Aaron Edsinger <aedsinger@hello-robot.com>
2022-07-20 22:56:55 -0700

    bump version

--- python/tools/RE1_dynamixel_calibrate_range.py
+++ python/tools/RE1_dynamixel_calibrate_range.py
@@ -15,16 +15,16 @@ group.add_argument("--head_tilt", help="Calibrate the head_tilt joint", action="
 group.add_argument("--wrist_yaw", help="Calibrate the wrist_yaw joint", action="store_true")
 args = parser.parse_args()
 
-if click.confirm('About to calibrate the head pan. Doing so will require you to recalibrated your URDF. Proceed?'):
-    if args.head_pan:
-        h=dxl.DynamixelHelloXL430('head_pan')
-    if args.head_tilt:
-        h = dxl.DynamixelHelloXL430('head_tilt')
-    if args.wrist_yaw:
-        h = dxl.DynamixelHelloXL430('wrist_yaw')
-    h.params['req_calibration']=1
-    if not h.startup():
-        exit(1)
-    h.home(single_stop=False,move_to_zero=True,delay_at_stop=1.0,save_calibration=True, set_homing_offset=False)
-    print('Recalibration done. Now redo the URDF calibration (see stretch_ros documentation)')
-    h.stop()
+
+if args.head_pan:
+    h=dxl.DynamixelHelloXL430('head_pan')
+if args.head_tilt:
+    h = dxl.DynamixelHelloXL430('head_tilt')
+if args.wrist_yaw:
+    h = dxl.DynamixelHelloXL430('wrist_yaw')
+h.params['req_calibration']=1
+if not h.startup():
+    exit(1)
+h.home(single_stop=False,move_to_zero=True,delay_at_stop=1.0,save_calibration=True, set_homing_offset=False)
+print('Recalibration done.)
+h.stop()
