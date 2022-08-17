5e24deb9d6023aad86e67f5746b3da889af437ea
Aaron Edsinger <aedsinger@hello-robot.com>
2022-08-17 12:28:29 -0700

    added preloading gains to eeprom to tools

--- python/tools/RE1_stepper_calibration_flash_to_YAML.py
+++ python/tools/RE1_stepper_calibration_flash_to_YAML.py
@@ -8,13 +8,11 @@ hu.print_stretch_re_use()
 
 parser=argparse.ArgumentParser(description='Pull encoder calibration from stepper flash and write to YAML')
 parser.add_argument('stepper_name', metavar='stepper_name', type=str, nargs=1,help='Provide the stepper name e.g.: hello-motor-lift')
-parser.add_argument('stepper_name', metavar='stepper_name', type=str, nargs=1,help='Provide the stepper name e.g.: hello-motor-lift')
 args=parser.parse_args()
 
 motor = stepper.Stepper('/dev/'+args.stepper_name[0])
 if not motor.startup():
     exit(1)
-
 
 data = motor.read_encoder_calibration_from_flash()
 print('Read data of len',len(data))
