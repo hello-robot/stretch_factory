15be43459a38786590d69ff285f4eb7d664ad266
Aaron Edsinger <aedsinger@hello-robot.com>
2023-03-09 15:20:48 -0800

    firmware trace ready

--- python/tools/REx_trace_firmware.py
+++ python/tools/REx_trace_firmware.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import argparse
 import click
 from colorama import Style
@@ -26,6 +26,7 @@ class TraceMgmt:
     Manage trace data
     """
     def __init__(self):
+        self.device_names = ['pimu', 'wacc', 'right_wheel', 'left_wheel', 'lift', 'arm']
         self.trace_directory = hu.get_stretch_directory() + 'log/trace_firmware'
         try:
             makedirs(self.trace_directory)
@@ -45,22 +46,26 @@ class TraceMgmt:
 
         while True:
             if len(trace_data):
-                click.secho('Current trace: Device %s | Start timestamp %f: ' % (
-                device_name, trace_data[0]['status']['timestamp']), fg="green", bold=True)
+                click.secho('Current trace: Device %s | Duration (s): %f | Start timestamp %f: ' % (
+                device_name, (trace_data[-1]['status']['timestamp']-trace_data[0]['status']['timestamp'])/1000000.0,trace_data[0]['status']['timestamp']), fg="green", bold=True)
             else:
                 click.secho('No trace loaded', fg="yellow", bold=True)
             print('')
             print(Style.BRIGHT + '############### MENU ################' + Style.RESET_ALL)
             print('Enter command. (q to quit)')
+            print('r: record trace on device')
             print('d: load trace from device')
             print('l: load trace from file')
             print('s: save trace to file')
             print('p: plot trace')
+            print('x: print trace to console')
             print('-------------------------------------')
             #try:
             r = input()
             if r == 'q' or r == 'Q':
                 return
+            elif r=='r':
+                trace_data,device_name=self.record_trace()
             elif r=='d':
                 trace_data,device_name=self.load_trace_from_device()
             elif r=='l':
@@ -69,10 +74,44 @@ class TraceMgmt:
                 self.save_trace(trace_data,device_name)
             elif r == 'p':
                 self.do_plot(trace_data,device_name)
+            elif r== 'x':
+                print(trace_data)
             else:
                 print('Invalid entry')
             # except(TypeError, ValueError):
             #     print('Invalid entry')
+
+    def record_trace(self):
+        device_id=self.get_device_id()
+        print('')
+        dd=None
+        device_name=self.device_names[device_id]
+        if  device_name== 'pimu':
+            dd = robot.pimu
+        if device_name == 'wacc':
+            dd = robot.wacc
+        if device_name == 'right_wheel':
+            dd =robot.base.right_wheel
+        if device_name == 'left_wheel':
+            dd =robot.base.left_wheel
+        if device_name == 'lift':
+            dd =robot.lift.motor
+        if device_name == 'arm':
+            dd =robot.arm.motor
+        if dd is not None:
+            input("Hit enter to begin recording")
+            dd.enable_firmware_trace()
+            robot.push_command()
+            print('\nRecording...\n')
+            input("Hit enter to end recording")
+            dd.disable_firmware_trace()
+            robot.push_command()
+
+        print('Reading trace back from recording. This may take a minute...')
+        trace_data = dd.read_firmware_trace()
+        if len(trace_data)==0:
+            print('No trace data found for %s'%device_name)
+        return trace_data, device_name
 
     def load_trace_from_file(self):
             # Retrieve sorted list of all trace files
@@ -91,28 +130,33 @@ class TraceMgmt:
                 print('No trace files available')
             return [],''
 
-    def load_trace_from_device(self):
-        device_names=['pimu','wacc','right_wheel','left_wheel','lift','arm']
+    def get_device_id(self):
         print(Style.BRIGHT + '############### Devices ################' + Style.RESET_ALL)
-        for i in range(len(device_names)):
-            print('%d: %s' % (i, device_names[i]))
+        for i in range(len(self.device_names)):
+            print('%d: %s' % (i, self.device_names[i]))
         print('')
         device_id = self.get_int([0, 5], 'device ID')
+
+        return device_id
+    def load_trace_from_device(self):
+        device_id=self.get_device_id()
+        print('')
+        print('Reading trace from device. This may take a minute...')
         print('')
         trace_data = []
-        device_name = device_names[device_id]
-        if device_names[device_id] == 'pimu':
-            trace_data = robot.pimu.read_trace()
-        if device_names[device_id] == 'wacc':
-            trace_data = robot.wacc.read_trace()
-        if device_names[device_id] == 'right_wheel':
-            trace_data = robot.base.right_wheel.read_trace()
-        if device_names[device_id] == 'left_wheel':
-            trace_data = robot.base.left_wheel.read_trace()
-        if device_names[device_id] == 'lift':
-            trace_data = robot.lift.motor.read_trace()
-        if device_names[device_id] == 'arm':
-            trace_data = robot.arm.motor.read_trace()
+        device_name = self.device_names[device_id]
+        if self.device_names[device_id] == 'pimu':
+            trace_data = robot.pimu.read_firmware_trace()
+        if self.device_names[device_id] == 'wacc':
+            trace_data = robot.wacc.read_firmware_trace()
+        if self.device_names[device_id] == 'right_wheel':
+            trace_data = robot.base.right_wheel.read_firmware_trace()
+        if self.device_names[device_id] == 'left_wheel':
+            trace_data = robot.base.left_wheel.read_firmware_trace()
+        if self.device_names[device_id] == 'lift':
+            trace_data = robot.lift.motor.read_firmware_trace()
+        if self.device_names[device_id] == 'arm':
+            trace_data = robot.arm.motor.read_firmware_trace()
         if len(trace_data)==0:
             print('No trace data found for %s'%device_name)
         return trace_data, device_name
@@ -147,6 +191,7 @@ class TraceMgmt:
         data=[]
         for t in trace_data:
             data.append(t['status'][field_name])
+        print('---------- PLOT DATA ----------')
         print(data)
         print('')
 
--- python/tools/REx_trace_robot.py
+++ python/tools/REx_trace_robot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import argparse
 import stretch_body.hello_utils as hu
 import click
