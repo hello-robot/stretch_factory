0456bb5c9334315c12b06dbcdc84151ff6bdd657
Aaron Edsinger <aedsinger@hello-robot.com>
2024-10-09 14:09:16 -0700

    wip

--- python/tools/REx_feetech_id_change.py
+++ python/tools/REx_feetech_id_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from future.builtins import input
-from stretch_body.dynamixel_XL430 import *
+from stretch_body.feetech_servo_sm import FeetechServoSM
 import argparse
 import stretch_body.device
 d = stretch_body.device.Device(name='dummy_device',req_params=False) # to initialize logging config
@@ -9,28 +9,28 @@ import stretch_body.hello_utils as hu
 hu.print_stretch_re_use()
 
 
-parser=argparse.ArgumentParser(description='Set the ID of a Dynamixel servo')
-parser.add_argument("usb_full_path", help="The full path to the dynamixel USB bus e.g.: /dev/hello-dynamixel-head")
+parser=argparse.ArgumentParser(description='Set the ID of a Feetech servo')
+parser.add_argument("usb_full_path", help="The full path to the dynamixel USB bus e.g.: /dev/ttyUSB0")
 parser.add_argument("id_from", help="The ID to change from", type=int)
 parser.add_argument("id_to", help="The ID to change to", type=int)
 args = parser.parse_args()
 
 try:
-    baud = DynamixelXL430.identify_baud_rate(args.id_from, args.usb_full_path)
+    baud = FeetechServoSM.identify_baud_rate(args.id_from, args.usb_full_path)
 except:
     print("Unable to detect baud at ID %d on bus %s"%(args.id_from,args.usb_full_path))
     exit(1)
 if baud !=-1:
-    m = DynamixelXL430(args.id_from, args.usb_full_path,baud=baud)
+    m = FeetechServoSM(args.id_from, args.usb_full_path,baud=baud)
     m.startup()
     if not m.do_ping():
         exit(0)
 
     input('Ready to change ID %d to %d. Hit enter to continue'%(args.id_from,args.id_to))
-    m.disable_torque()
+    #m.disable_torque()
     m.set_id(args.id_to)
 
-    m = DynamixelXL430(args.id_to, args.usb_full_path,baud=baud)
+    m = FeetechServoSM(args.id_to, args.usb_full_path,baud=baud)
     m.startup()
     if not m.do_ping():
         print('Failed to set new ID')
--- python/tools/REx_feetech_id_scan.py
+++ python/tools/REx_feetech_id_scan.py
@@ -1,40 +1,30 @@
 #!/usr/bin/env python
 
-from stretch_body.dynamixel_XL430 import DynamixelXL430, DynamixelCommError
+from stretch_body.feetech_servo_sm import FeetechServoSM, FeetechCommError
 import argparse
-
+import sys
 import stretch_body.hello_utils as hu
 hu.print_stretch_re_use()
 
 import stretch_body.device
 d = stretch_body.device.Device(name='dummy_device',req_params=False) # to initialize logging config
 
-parser=argparse.ArgumentParser(description='Scan a dynamixel bus by ID for servos')
-parser.add_argument("usb_full_path", help="The full path to dynamixel USB bus e.g.: /dev/hello-dynamixel-head")
+parser=argparse.ArgumentParser(description='Scan a feetech bus by ID for servos')
+parser.add_argument("usb_full_path", help="The full path to dynamixel USB bus e.g.: /dev/ttyUSB0")
 args = parser.parse_args()
 
 m=None
 nfind=0
 try:
-    print('Scanning bus %s' % (args.usb_full_path))
-    for id in range(25):
-        print('Checking ID %d'%id)
-        baud = DynamixelXL430.identify_baud_rate(id, args.usb_full_path)
-        if baud !=-1:
-            m = DynamixelXL430(id, args.usb_full_path,baud=baud)
-            m.startup()
-            if not m.hw_valid:
-                print('Unable to open bus %s. Exiting'%args.usb_full_path)
-                exit(1)
-            try:
-                nfind=nfind+1
-            except DynamixelCommError:
-                print("Ping failed for ID %d and baud %d: "%(str(id),baud))
-                continue
-            m.do_ping()
-            m.stop()
+    baudrates=[115200,1000000]
+    for b in baudrates:
+        print('------------')
+        print('Scanning bus %s at baudrate %d' % (args.usb_full_path,b))
+        result=FeetechServoSM.list_servos(args.usb_full_path,b)
+        if len(result):
+            print('Found %d  servos on bus %s' % (len(result), args.usb_full_path))
+            break
 except (KeyboardInterrupt, SystemExit):
-    if m is not None:
-        m.stop()
+    print('Error while scanning bus')
 
-print('Found %d  servos on bus %s'%(nfind,args.usb_full_path))
+
